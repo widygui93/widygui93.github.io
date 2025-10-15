6abb78e3120f016321dc12c07ef260acb71abbb4
Widy Guilias <widyguilias@gmail.com>
2025-10-15 16:14:36 +0700

    add script for open whatsapp window for consultation

---
+++ script.js
@@ -0,0 +1,3 @@
+function konsultasi() {
+	window.open('https://wa.me/6283175778119?text=Halo, saya ingin konsultasi tentang jasa pembuatan website landing page ataupun pembuatan sistem terkait bisnis saya','_blank','noopener,noreferrer')
+}
